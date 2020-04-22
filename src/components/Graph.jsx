2a6a8b835ffdbf5bd328a977bac207f6a88d53e1
Pablo Tovar <pjtf93@gmail.com>
2020-04-22 18:04:31 -0400

    Minor fixes

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 // import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Header from '../components/Header';
 import Content from '../components/Content';
-import Side from '../components/Side';
-import Footer from '../components/Footer';
 
 // import '../assets/styles/App.css';
 
